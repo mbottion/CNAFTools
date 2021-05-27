5ed3a73c9394ac254a03dcfdc85c8359f408c761
mbottion <84838458+mbottion@users.noreply.github.com>
2021-05-27 13:33:28 +0200

    Create OPARegis.sql

---
+++ OPARegis.sql
@@ -0,0 +1,47 @@
+set serveroutput on
+begin
+  if (upper('&1') in ('USAGE','HELP','-?','-H')
+  then
+    raise_application_error(-20000,'Usage:
+    
+    Parameters :
+       P1: Analysis start date (dd/mm/yyyy [hh24:mi:ss]) - Default : Midnight today
+       P2: Analysis end date   (dd/mm/yyyy [hh24:mi:ss]) - Default : now
+       
+       );'
+end ;
+/
+
+define start_date="nvl('&1',to_char(sysdate,'dd/mm/yyyy'))"
+define end_date="nvl('&2',to_char(sysdate,'dd/mm/yyyy hh24:mi:ss'))"
+
+-- -----------------------------------------------------------------
+-- SQL
+-- -----------------------------------------------------------------
+SELECT
+        /*+PARALLEL */
+        ENGINE_NAME           ,
+        to_char(START_CALL_ENGINE,'dd/mm/yyyy hh24:mi:ss') START_CALL_ENGINE,
+        "CasesRead"           ,
+        "ProcessorDurationSec",
+        TOTAL_OPA
+FROM
+        (
+                SELECT  ID_TDB_INTERFACE                                                                                                                                         ,
+                        ENGINE_NAME                                                                                                                                              ,
+                        START_CALL_ENGINE,
+                        END_CALL_ENGINE                                                                                                                                          ,
+                        EXTRACT(SECOND FROM(END_CALL_ENGINE                          -START_CALL_ENGINE) DAY TO SECOND)                                 AS TOTAL_OPA             ,
+                        TO_NUMBER(REPLACE(TRIM(SUBSTR(SUMMARY,INSTR(SUMMARY,':',1, 1)+1, INSTR(SUMMARY,'-',1, 2) -INSTR(SUMMARY,':',1, 1)-1)),',','.')) AS "CasesRead"           ,
+                        TO_NUMBER(REPLACE(TRIM(SUBSTR(SUMMARY,INSTR(SUMMARY,':',1, 2)+1, INSTR(SUMMARY,'-',1, 3) -INSTR(SUMMARY,':',1, 2)-1)),',','.')) AS "CasesProcessed"      ,
+                        TO_NUMBER(REPLACE(TRIM(SUBSTR(SUMMARY,INSTR(SUMMARY,':',1, 3)+1, INSTR(SUMMARY,'-',1, 4) -INSTR(SUMMARY,':',1, 3)-1)),',','.')) AS "CasesIgnored"        ,
+                        TO_NUMBER(REPLACE(TRIM(SUBSTR(SUMMARY,INSTR(SUMMARY,':',1, 4)+1, INSTR(SUMMARY,'-',1, 5) -INSTR(SUMMARY,':',1, 4)-1)),',','.')) AS "ProcessorDurationSec",
+                        TO_NUMBER(REPLACE(TRIM(SUBSTR(SUMMARY,INSTR(SUMMARY,':',1, 5)+1, LENGTH(SUMMARY) -INSTR(SUMMARY,':',1, 5))),',','.'))           AS "ProcessorCasesPerSec"
+                FROM    RDACCENTURE.V_S_DASHBOARD_HIST_OPA
+                WHERE   STATUS ='Completed'
+                        AND SUMMARY NOT LIKE '%ORA-%'
+--                        AND -- Filtre sur les traitements
+--                        START_BUILD_JSON >= to_date('&start_date','dd/mm/yyyy hh24:mi:ss') AND START_BUILD_JSON <= to_date('&end_date','dd/mm/yyyy hh24:mi:ss')
+        )
+ORDER BY START_CALL_ENGINE ASC
+/
