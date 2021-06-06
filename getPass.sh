72d991e126a32fe843800008aa72efa97606f5c1
mbottion <84838458+mbottion@users.noreply.github.com>
2021-06-06 12:03:55 +0200

    Updated by runSQL.sh

---
+++ getPass.sh
@@ -0,0 +1,25 @@
+info=${1:-passwd}
+if [ -d /acfs01/dbaas_acfs/$ORACLE_UNQNAME/db_wallet ]
+then
+  dir=/acfs01/dbaas_acfs/$ORACLE_UNQNAME/db_wallet
+elif [ -d /acfs01/dbaas_acfs/$ORACLE_SID/db_wallet ]
+then
+  dir=/acfs01/dbaas_acfs/$ORACLE_SID/db_wallet
+elif [ -d /acfs01/dbaas_acfs/$(echo $ORACLE_SID|sed -e "s;.$;;")/db_wallet ]
+then
+  dir=/acfs01/dbaas_acfs/$(echo $ORACLE_SID|sed -e "s;.$;;")/db_wallet
+else
+  echo DB_WALLET Introuvable
+  exit 1
+fi
+echo
+echo "==================================="
+echo
+mkstore -wrl $dir -list
+echo
+echo "==================================="
+echo
+mkstore -wrl $dir -viewEntry passwd
+echo
+echo "==================================="
+echo
